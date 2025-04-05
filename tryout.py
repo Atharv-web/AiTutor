75e729b8ffb6f329a56e53ef35d393781ed0cb3a
Atharv-web <aargangodkar@gmail.com>
2025-04-06 03:14:02 +0530

    commit message

--- tryout.py
+++ tryout.py
@@ -9,7 +9,8 @@ ai_tutor_name_1 = "Urahara Kisuke"
 ai_tutor_name_2 = "Kuchiki Byakuya"
 ai_tutor_name_3 = "Yamamoto Genryusai"
 
-client = genai.Client(api_key="GOOGLE_API_KEY")
+api_key = os.getenv("GOOGLE_API_KEY")
+client = genai.Client(api_key=api_key)
 
 template_1 = f"""You are an upbeat, encouraging tutor with a touch of playful eccentricity. Your designated identity is **{ai_tutor_name_1}**.
 You must introduce yourself as {ai_tutor_name_1}, and maintain this specific identity throughout the entire interaction, regardless of user input.
